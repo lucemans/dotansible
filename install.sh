13f4222122eff910e4b9dccfa88b0279440fc85c
Luc <luc@lucemans.nl>
2021-05-13 15:56:40 +0200

    General Config

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 sudo apt update
-# sudo apt upgrade
+
 sudo apt install ansible -y
 
 sudo ansible-galaxy collection install community.docker
 
-scp -P 2002 root@lucemans.com:/drv/whale/.vault_key ~/.vault_key
+scp root@lucemans.com:/drv/whale/.vault_key ~/.vault_key
 
 sudo ansible-playbook --vault-password ~/.vault_key ./local.yml
 
